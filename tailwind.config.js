5dd01ff4d81a94ec24dbf370801d0592390196aa
Debashis Roy <goodquote39@hotmail.com>
2023-02-27 09:20:44 +0600

    fixed some issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  purge:['./src/index.html'],
+  purge:['./index.html'],
   theme: {
     extend: {},
   },
